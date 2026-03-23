9072c4711c9d59c13f555bf54408e332c023031d
justina-peciulyte <justina.peciulyte@mif.stud.vu.lt>
2026-03-24 00:21:45 +0200

    Fixed cause of ValueError after updating other modules
    Added proximity constant and modified track parameters.

--- anomaly_B.py
+++ anomaly_B.py
@@ -1,4 +1,5 @@
 import math
+from collections import defaultdict
 
 # Anomaly B (Loitering & Transfers): 
 # Detect two distinct, valid MMSI numbers located within 500 meters of each other, 
@@ -6,6 +7,7 @@ import math
 
 LOITERING_SPEED_THRESHOLD = 1  # Knots
 LOITERING_DURATION = 2*3600 # Seconds
+PROXIMITY_METERS = 500
 
 # Not finished yet !
 # Needs to inclue the distance check between two vessels, so only checks loitering for one vessel.
@@ -14,8 +16,8 @@ def loitering_check(track):
     """Checks if the vessel is moving at a slow speed for an extended period."""
     start_time = None
 
-    for timestamp, lat, lon, sog, draught in track:
-        if not math.isnan(sog) and sog < LOITERING_SPEED_THRESHOLD:
+    for timestamp, lat, lon, sog, draught, is_carryover in track:
+        if sog is not None and not math.isnan(sog) and sog < LOITERING_SPEED_THRESHOLD:
             if start_time is None:
                 start_time = timestamp
             duration = (timestamp - start_time).total_seconds()
